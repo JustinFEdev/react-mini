a40aa6f31e45763647528b8addd8ad36fb8548f6
Justin_supertree <justin1688@supertree.co>
2021-11-28 16:41:49 +0900

    feagture/mainpage

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,33 +5,31 @@ import "../styles/layout.css";
 import battle_video from "../sources/videos/pubg.mp4";
 import nexon_video from "../sources/videos/nexon.mp4";
 import odin_video from "../sources/videos/odin.mp4";
-const battle_img =
-  "https://iconape.com/wp-content/files/kv/247817/svg/247817.svg";
-const kakao_img =
-  "https://webofficial.kakaogames.com/live/official/file/news/20201104/2237.png";
+const battle_img = "https://iconape.com/wp-content/files/kv/247817/svg/247817.svg";
+const kakao_img = "https://webofficial.kakaogames.com/live/official/file/news/20201104/2237.png";
 const nexons_img = "https://ssl.nexon.com/S2/p3/main/nexon.png";
 
 const Main = () => {
   const info = [
     {
+      id: 1,
       name: "Krafton",
       page: "Krafton",
-      id: 1,
       img: battle_img,
       video: battle_video,
       type: "video/mp4",
     },
     {
+      id: 2,
       name: "Nexon",
       page: "Nexon",
-      id: 2,
       img: nexons_img,
       video: nexon_video,
     },
     {
+      id: 3,
       name: "Kakao",
       page: "Kakao",
-      id: 3,
       img: kakao_img,
       video: odin_video,
     },
